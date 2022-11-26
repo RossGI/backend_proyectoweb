c1652e91b2d40c1101e8c06cda42a2180c854037
RossGI <mrginiguez@gmail.com>
2022-11-25 23:10:40 -0600

    Formato en modelo empleados

--- src/modelos/empleados.js
+++ src/modelos/empleados.js
@@ -1,43 +1,16 @@
 const {Schema,model} = require('mongoose');
 
 const schema = new Schema({
-    nombre: {
-        type: String
-    },
-
-    apellido:{
-        type: String
-    },
-
-    correo:{
-        type: String
-    },
-
-    telefono:{
-        type: String
-    },
-
-    contraseña: {
-        type: String
-    },
-
-    rol: {
-        type: String
-    },
-
-    direccion:{
-        type: String
-    },
-
-    rfc: {
-        type: String
-    },
-
-    status:{
-        type: Number,
-        default: 1
-    }
-
+    nombre:     {type: String},
+    apellido:   {type: String},
+    correo:     {type: String},
+    telefono:   {type: String},
+    contraseña: {type: String},
+    rol:        {type: String},
+    direccion:  {type: String},
+    rfc:        {type: String},
+    status:     {type: Number, default: 1}
 });
+
 
 module.exports = model('empleados',schema);
